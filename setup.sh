a62ba8edd08454539f4ca8996c2eaa0c4c682871
Alexander David Frick <Alex313031@gmail.com>
2021-10-26 13:51:28 -0500

    Add components dir

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 cp -r -v build/. $HOME/chromium/src/build/ &&
 cp -r -v chrome/. $HOME/chromium/src/chrome/ &&
+cp -r -v components/. $HOME/chromium/src/components &&
 cp -r -v content/. $HOME/chromium/src/content/ &&
 cp -r -v media/. $HOME/chromium/src/media/ &&
 cp -r -v ui/. $HOME/chromium/src/ui/ &&
