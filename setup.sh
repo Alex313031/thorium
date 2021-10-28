583763d326e94c318c642e8f576051985ac30886
Alexander David Frick <Alex313031@gmail.com>
2021-10-28 08:47:38 -0500

    Include net dir.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cp -r -v chrome/. $HOME/chromium/src/chrome/ &&
 cp -r -v components/. $HOME/chromium/src/components &&
 cp -r -v content/. $HOME/chromium/src/content/ &&
 cp -r -v media/. $HOME/chromium/src/media/ &&
+cp -r -v net/. $HOME/chromium/src/net/ &&
 cp -r -v ui/. $HOME/chromium/src/ui/ &&
 cp -r -v v8/. $HOME/chromium/src/v8/ &&
 
