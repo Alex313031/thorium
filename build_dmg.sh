24fc92b1956df5f3123e81b56f2f37a3ecbd76a2
midzer <midzer@gmail.com>
2022-01-07 11:18:45 +0100

    replace Chromium -> Throrium

--- build_dmg.sh
+++ build_dmg.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-xattr -csr out/thorium/Chromium.app
+xattr -csr out/thorium/Thorium.app
 
-codesign --force --deep --sign - out/thorium/Chromium.app
+codesign --force --deep --sign - out/thorium/Thorium.app
 
 chrome/installer/mac/pkg-dmg \
-  --sourcefile --source out/thorium/Chromium.app \
+  --sourcefile --source out/thorium/Thorium.app \
   --target "out/thorium/Thorium_MacOS.dmg" \
   --volname Thorium --symlink /Applications:/Applications \
   --format UDBZ --verbosity 2
-
