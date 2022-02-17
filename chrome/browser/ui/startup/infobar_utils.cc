fac2538478f5b86d3eca572613fe2dd3d1bb7256
Alexander David Frick <Alex313031@gmail.com>
2022-02-17 11:57:09 -0600

    Update infobar_utils.cc

--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -1,6 +1,6 @@
 // Copyright 2022 The Chromium Authors and Alex313031. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file. 
 
 #include "chrome/browser/ui/startup/infobar_utils.h"
 
