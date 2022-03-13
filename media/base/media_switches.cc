1a69c03958f24c3b7f7ff977872ca65f6bc42176
Alexander David Frick <Alex313031@gmail.com>
2022-03-13 13:50:17 -0500

    Enable SODA on AVX builds.

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -949,9 +949,7 @@ bool IsLiveCaptionFeatureEnabled() {
 #if BUILDFLAG(IS_LINUX)
   // Check if the CPU has the required instruction set to run the Speech
   // On-Device API (SODA) library.
-  static bool has_sse41 = base::CPU().has_sse41();
-  if (!has_sse41)
-    return false;
+    return true;
 #endif
 
 #if BUILDFLAG(IS_WIN) && defined(ARCH_CPU_ARM64)
