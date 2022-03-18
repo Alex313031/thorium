1dda28c375dbeb7c6e5d34e757f6e68d8a5f8227
Alexander David Frick <Alex313031@gmail.com>
2022-03-18 16:24:51 -0500

    Update profile_shortcut_manager_win.cc

--- chrome/browser/profiles/profile_shortcut_manager_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_win.cc
@@ -891,7 +891,7 @@ void ProfileShortcutManagerWin::HasProfileShortcuts(
   base::PostTaskAndReplyWithResult(
       base::ThreadPool::CreateCOMSTATaskRunner({base::MayBlock()}).get(),
       FROM_HERE, base::BindOnce(&HasAnyProfileShortcuts, profile_path),
-      (std::move(callback));
+      std::move(callback));
 }
 
 void ProfileShortcutManagerWin::GetShortcutProperties(
