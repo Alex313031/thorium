fd9eedccff101d4f425deb7ac820b284af7a1423
Alexander David Frick <Alex313031@gmail.com>
2022-04-09 01:19:50 -0500

    Add echos

--- trunk.sh
+++ trunk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Rebasing/Syncing and running hooks..." &&
+
 cd $HOME/chromium/src/v8/ &&
 
 git checkout -f origin/main &&
@@ -16,10 +18,16 @@ gclient sync --with_branch_heads --with_tags -f -R -D &&
 
 gclient runhooks &&
 
+echo "Done!" &&
+
+echo "Downloading PGO Profiles for Linux, Windows, and Mac..." &&
+
 python tools/update_pgo_profiles.py --target=linux update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 
 python tools/update_pgo_profiles.py --target=win64 update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 
 python tools/update_pgo_profiles.py --target=mac update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
+
+echo "Done!  You can now run ./setup.sh."
 
 exit 0
