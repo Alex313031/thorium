a75c8dbce1097e7d407b6b4d2259033171de7c18
Alexander David Frick <Alex313031@gmail.com>
2022-04-30 01:08:36 -0500

    Add widevine cdm cu patch

---
+++ third_party/widevine/cdm/widevine_cdm_version.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is a stand-in for a generated file that is available when the
+// Widevine CDM is available.
+
+#ifndef WIDEVINE_CDM_VERSION_H_
+#define WIDEVINE_CDM_VERSION_H_
+
+// If the Widevine CDM is available define the following:
+//  - WIDEVINE_CDM_VERSION_STRING (with the version of the CDM that's available
+//    as a string, e.g., "1.0.123.456").
+
+#define WIDEVINE_CDM_VERSION_STRING "undefined"
+
+#endif  // WIDEVINE_CDM_VERSION_H_
