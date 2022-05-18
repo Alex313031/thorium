9cc4e87cd04f4ab734a2105730c15b16be913e32
Alexander David Frick <Alex313031@gmail.com>
2022-05-18 16:26:46 -0500

    Update build.bat

--- win_scripts/build.bat
+++ win_scripts/build.bat
@@ -3,7 +3,11 @@
 
 cd C:\src\chromium\src\ &&
 
+echo "Setting NINJA_SUMMARIZE_BUILD=1" &&
+
 set NINJA_SUMMARIZE_BUILD=1 &&
+
+echo "Building Thorium..." &&
 
 autoninja -C out\thorium chrome chromedriver content_shell setup mini_installer views_examples_with_content -j 8
 
