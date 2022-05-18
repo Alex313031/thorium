e4995b271ba91de5453c758b147549dc53eb4d74
Alexander David Frick <Alex313031@gmail.com>
2022-05-18 16:25:56 -0500

    Update trunk.bat

--- win_scripts/trunk.bat
+++ win_scripts/trunk.bat
@@ -1,6 +1,8 @@
 ## Windows native build trunk script.
 # Copyright (c) 2022 Alex313031. 
 
+echo "Rebasing/Syncing and running hooks..." &&
+
 cd C:\src\chromium\src\v8 &&
 
 git checkout -f origin/main &&
@@ -16,6 +18,8 @@ git fetch --tags &&
 gclient sync --with_branch_heads --with_tags -f -R -D &&
 
 gclient runhooks &&
+
+echo "Downloading PGO Profile for Win64..." &&
 
 python tools\update_pgo_profiles.py --target=win64 update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 
