4c782dc2d289bc84700a3a712b4408ceb2044f56
Alexander David Frick <Alex313031@gmail.com>
2022-05-21 09:31:26 -0500

    Update features.cc

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1355,14 +1355,6 @@ const base::Feature kElementSuperRareData{"ElementSuperRareData",
 const base::Feature kClientHintsPartitionedCookies{
     "ClientHintsPartitionedCookies", base::FEATURE_DISABLED_BY_DEFAULT};
 
-// If enabled, the memory limit used for tiles is scaled by
-// `kScaleTileMemoryLimitFactor`.
-const base::Feature kScaleTileMemoryLimit{"ScaleTileMemoryLimit",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::FeatureParam<double> kScaleTileMemoryLimitFactor{
-    &kScaleTileMemoryLimit, "Factor", 1.0};
-
 const base::Feature kDurableClientHintsCache{"DurableClientHintsCache",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
@@ -1471,6 +1463,15 @@ const base::Feature kSubstringSetTreeForAttributeBuckets{
 
 const base::Feature kPendingBeaconAPI{"PendingBeaconAPI",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kSimulateClickOnAXFocus {
+  "SimulateClickOnAXFocus",
+#if BUILDFLAG(IS_WIN)
+      base::FEATURE_ENABLED_BY_DEFAULT
+#else
+      base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+};
 
 }  // namespace features
 }  // namespace blink
