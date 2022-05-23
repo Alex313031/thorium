e2741e5da0579a811d59bfbccbcda88e347e19df
Alexander David Frick <Alex313031@gmail.com>
2022-05-23 03:13:52 -0700

    Update shortcut.h

--- base/win/shortcut.h
+++ base/win/shortcut.h
@@ -63,6 +63,7 @@ struct BASE_EXPORT ShortcutProperties {
   }
 
   void set_arguments(const std::wstring& arguments_in) {
+	L"--autoplay-policy=user-gesture-required";
     arguments = arguments_in;
     options |= PROPERTIES_ARGUMENTS;
   }
