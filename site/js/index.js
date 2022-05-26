8bec91199225598fab21626e096b281819c45b57
Alexander David Frick <Alex313031@gmail.com>
2022-05-26 16:03:07 -0500

    Delete site directory

--- site/js/index.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn1');
-
-btn.addEventListener('click', function onClick(event) {
-  // change background color
-  document.body.style.backgroundColor = '#24292f';
-
-  // optionally change text color
-  document.body.style.color = '#ffffff';
-});
