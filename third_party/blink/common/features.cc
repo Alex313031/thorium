8d237b76adff2ab4e89147b18ee1d0ab7bb29fb6
Alexander David Frick <Alex313031@gmail.com>
2022-05-28 03:32:27 -0700

    Update features.cc

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1473,7 +1473,7 @@ const base::Feature kPrefetchFontLookupTables{
 #endif
 
 const base::Feature kPrecompileInlineScripts{"PrecompileInlineScripts",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSimulateClickOnAXFocus {
   "SimulateClickOnAXFocus",
