61929765a513ea537c01d60831f779e9fe7de9ed
Alexander David Frick <Alex313031@gmail.com>
2022-05-28 14:06:32 -0700

    Delete website directory

--- website/README.md
+++
@@ -1,7 +0,0 @@
-# https://thorium.rocks/ Source
-
-## Thorium Website Source
-
-This is the source code for the new Thorium Browser Website.
-
-Kudos to @midzer
--- website/js/button.js
+++
@@ -1,7 +0,0 @@
-// Dark mode button.
-
-document.querySelector('button').onclick = function() {
-        useDark = !useDark;
-        toggleDarkMode(useDark);
-        localStorage.setItem('dark-mode', useDark);
-    }
