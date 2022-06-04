66bbb48b6a315d833b5b9c9220006cf57e0b1a45
Alexander David Frick <Alex313031@gmail.com>
2022-06-04 05:46:34 -0500

    Add files via upload

---
+++ components/ukm/scheme_constants.cc
@@ -0,0 +1,13 @@
+// Copyright 2022 The Chromium Authors and Alex313031. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/ukm/scheme_constants.h"
+
+namespace ukm {
+
+const char kAppScheme[] = "app";
+const char kChromeUIScheme[] = "thorium";
+const char kExtensionScheme[] = "chrome-extension";
+
+}  // namespace ukm
