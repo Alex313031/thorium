ca2698833209bcc323a3b2956e7b970fdda2c8a0
Alexander David Frick <Alex313031@gmail.com>
2022-06-06 15:57:25 -0700

    Update check_avx.sh

--- check_avx.sh
+++ check_avx.sh
@@ -7,6 +7,14 @@ program="Thorium Browser." # The program we are checking for.
 YEL='\033[1;33m' # Yellow
 RED='\033[1;31m' # Red
 GRE='\033[1;32m' # Green
+c0=$'\033[0m' # Reset Text
+bold=$'\033[1m' # Bold Text
+underline=$'\033[4m' # Underline Text
+
+# Error handling
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "${RED}Failed $*"; }
 
 script_name=${0##*/}
 
