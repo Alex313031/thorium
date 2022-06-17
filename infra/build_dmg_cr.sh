63b5ae66102960974e3e003d0a749f1c1988eea8
Alexander David Frick <Alex313031@gmail.com>
2022-06-17 08:11:35 -0700

    Create build_dmg_cr.sh

---
+++ infra/build_dmg_cr.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright (c) 2022 Alex313031 and Midzer.
+
+YEL='\033[1;33m' # Yellow
+RED='\033[1;31m' # Red
+GRE='\033[1;32m' # Green
+c0=$'\033[0m' # Reset Text
+bold=$'\033[1m' # Bold Text
+underline=$'\033[4m' # Underline Text
+
+# Error handling
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "${RED}Failed $*"; }
+
+printf "\n" &&
+printf "${YEL}Building .dmg of Chromium...\n" &&
+printf "${GRE}\n" &&
+
+# Fix file attr
+xattr -csr out/thorium/Chromium.app &&
+
+# Sign binary
+codesign --force --deep --sign - out/thorium/Chromium.app &&
+
+# Build dmg package
+chrome/installer/mac/pkg-dmg --sourcefile --source out/thorium/Chromium.app --target "out/thorium/Chromium_MacOS.dmg" --volname Chromium --symlink /Applications:/Applications --format UDBZ --verbosity 2 &&
+  
+printf "${GRE}.DMG Build Completed. ${YEL}Installer at //out/thorium/Chromium*.dmg\n" &&
+tput sgr0 &&
