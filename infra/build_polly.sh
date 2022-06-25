9eca14f132d6bdc75fe61fc779051e64cb929534
Alexander David Frick <Alex313031@gmail.com>
2022-06-25 11:45:41 -0500

    Update build_polly.sh

--- infra/build_polly.sh
+++ infra/build_polly.sh
@@ -4,6 +4,6 @@
 
 ## Clones latest LLVM being used by the Chromium Project, and builds a local toolchain with Polly to use the Polly optimizations in the main Thorium BUILD.gn
 
-cd ~/chromium/src
+cd ~/chromium/src &&
 
 python3 tools/clang/scripts/build.py --without-android --without-fuchsia --disable-asserts --gcc-toolchain=/usr --bootstrap --thinlto
