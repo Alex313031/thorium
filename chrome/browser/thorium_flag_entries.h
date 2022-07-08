738b2f2fbb61cdbfa2dc87a55fe2f14fe174fb5d
Alexander David Frick <Alex313031@gmail.com>
2022-07-07 22:18:52 -0500

    Update thorium_flag_entries.h

--- chrome/browser/thorium_flag_entries.h
+++ chrome/browser/thorium_flag_entries.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 The ungoogled-chromium Authors and Alex313031. All rights reserved.
+// Copyright (c) 2022 Alex313031. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,10 +6,18 @@
 #define CHROME_BROWSER_THORIUM_FLAG_ENTRIES_H_
     {"custom-ntp",
      "Custom New Tab Page",
-     "Allows setting a custom URL for the new tab page.  Value can be internal (e.g. `about:blank`), external (e.g. `example.com`), or local (e.g. `file:///tmp/startpage.html`).  This applies for incognito windows as well when not set to a `chrome://` internal page.",
+     "Allows setting a custom URL for the New Tab Page (NTP). Value can be internal (e.g. `about:blank`), external (e.g. `example.com`), or local (e.g. `file:///tmp/startpage.html`). This applies for incognito windows as well when not set to a `chrome://` internal page.",
      kOsDesktop, ORIGIN_LIST_VALUE_TYPE("custom-ntp", "")},
     {"hide-sidepanel-button",
-     "Hide SidePanel Button",
+     "Hide Side Panel Button",
      "Hides the Thorium Side Panel Button.",
      kOsDesktop, SINGLE_VALUE_TYPE("hide-sidepanel-button")},
+     {"force-dark-mode",
+     "Force Dark Mode",
+     "Enables Dark Mode (like in Incognito windows) for all Thorium instances.",
+     kOsDesktop, SINGLE_VALUE_TYPE("force-dark-mode")},
+     {"no-autoplay",
+     "Disable AutoPlay",
+     "Disables AutoPlay for all Thorium instances.",
+     kOsDesktop, SINGLE_VALUE_TYPE("no-autoplay")},
 #endif  // CHROME_BROWSER_THORIUM_FLAG_ENTRIES_H_
