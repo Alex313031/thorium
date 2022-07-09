690d76b1da9c53dbbd6c415c72cc614c3adf07ff
Alexander David Frick <Alex313031@gmail.com>
2022-07-09 18:46:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -55,7 +55,7 @@ cp -r -v src/pak_src/bin/pak $HOME/chromium/src/out/thorium/ &&
 cp -r -v src/pak_src/bin/pak-win/. $HOME/chromium/src/out/thorium/ &&
 
 # Workaround for DevTools
-cp -r -v src/third_party/devtools-frontend/src/front_end/Images/src/chromeSelectDark.svg $HOME/chromium/src/out/thorium/gen/third_party/devtools-frontend/src/front_end/Images/
+cp -r -v src/third_party/devtools-frontend/src/front_end/Images/src/chromeSelectDark.svg $HOME/chromium/src/out/thorium/gen/third_party/devtools-frontend/src/front_end/Images/ &&
 
 printf "${GRE}Done!\n" &&
 printf "\n" &&
