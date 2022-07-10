03cce90162e73ca0b6442b73592edc3021064639
Alexander David Frick <Alex313031@gmail.com>
2022-07-09 19:08:44 -0500

    Update setup.bat

--- win_scripts/setup.bat
+++ win_scripts/setup.bat
@@ -7,27 +7,27 @@ mkdir C:\src\chromium\src\out\thorium &&
 
 echo "Copying Thorium source files over the Chromium tree..." &&
 
-copy ..\ash\* C:\src\chromium\src\ash &&
-copy ..\base\* C:\src\chromium\src\base &&
-copy ..\build\* C:\src\chromium\src\build &&
-copy ..\chrome\* C:\src\chromium\src\chrome &&
-copy ..\components\* C:\src\chromium\src\components &&
-copy ..\extensions\* C:\src\chromium\src\extensions &&
-copy ..\content\* C:\src\chromium\src\content &&
-copy ..\media\* C:\src\chromium\src\media &&
-copy ..\net\* C:\src\chromium\src\net &&
-copy ..\sandbox\* C:\src\chromium\src\sandbox &&
-copy ..\third_party\* C:\src\chromium\src\third_party &&
-copy ..\tools\* C:\src\chromium\src\tools &&
-copy ..\ui\* C:\src\chromium\src\ui &&
-copy ..\v8\* C:\src\chromium\src\v8 &&
-copy ..\content_shell\* C:\src\chromium\src\out\thorium &&
-copy ..\pak_src\bin\pak-win\* C:\src\chromium\src\out\thorium &&
+copy ..\src\ash\* C:\src\chromium\src\ash &&
+copy ..\src\base\* C:\src\chromium\src\base &&
+copy ..\src\build\* C:\src\chromium\src\build &&
+copy ..\src\chrome\* C:\src\chromium\src\chrome &&
+copy ..\src\components\* C:\src\chromium\src\components &&
+copy ..\src\extensions\* C:\src\chromium\src\extensions &&
+copy ..\src\content\* C:\src\chromium\src\content &&
+copy ..\src\media\* C:\src\chromium\src\media &&
+copy ..\src\net\* C:\src\chromium\src\net &&
+copy ..\src\sandbox\* C:\src\chromium\src\sandbox &&
+copy ..\src\third_party\* C:\src\chromium\src\third_party &&
+copy ..\src\tools\* C:\src\chromium\src\tools &&
+copy ..\src\ui\* C:\src\chromium\src\ui &&
+copy ..\src\v8\* C:\src\chromium\src\v8 &&
+copy ..\src\content_shell\* C:\src\chromium\src\out\thorium &&
+copy ..\src\pak_src\bin\pak-win\* C:\src\chromium\src\out\thorium &&
 
 set NINJA_SUMMARIZE_BUILD=1 &&
 
 echo "Dropping you to C:\src\chromium\src" &&
 
-cd C:\chromium\src
+cd C:\src\chromium\src
 
 exit 0
