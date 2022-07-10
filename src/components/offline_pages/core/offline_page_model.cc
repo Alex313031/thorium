b495839b6671921bf644617460433d49840203eb
Alexander David Frick <Alex313031@gmail.com>
2022-07-10 03:08:24 -0500

    Add files via upload

---
+++ src/components/offline_pages/core/offline_page_model.cc
@@ -0,0 +1,32 @@
+// Copyright 2022 The Chromium Authors and Alex313031. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_pages/core/offline_page_model.h"
+
+#include "url/gurl.h"
+
+namespace offline_pages {
+
+const int64_t OfflinePageModel::kInvalidOfflineId;
+
+OfflinePageModel::SavePageParams::SavePageParams()
+    : proposed_offline_id(OfflinePageModel::kInvalidOfflineId),
+      is_background(false),
+      use_page_problem_detectors(false) {}
+
+OfflinePageModel::SavePageParams::SavePageParams(const SavePageParams& other) =
+    default;
+
+OfflinePageModel::SavePageParams::~SavePageParams() = default;
+
+// static
+bool OfflinePageModel::CanSaveURL(const GURL& url) {
+  return url.is_valid();
+}
+
+OfflinePageModel::OfflinePageModel() = default;
+
+OfflinePageModel::~OfflinePageModel() = default;
+
+}  // namespace offline_pages
