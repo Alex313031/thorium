f2be1a9c16e4b05898a14a153f25b43cb6028444
Alexander David Frick <Alex313031@gmail.com>
2022-07-17 19:46:27 -0500

    Add files via upload

---
+++ src/chrome/browser/thorium_flag_choices.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2022 Alex313031. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_THORIUM_FLAG_CHOICES_H_
+#define CHROME_BROWSER_THORIUM_FLAG_CHOICES_H_
+const FeatureEntry::Choice kShowAvatarButtonChoices[] = {
+    {flags_ui::kGenericExperimentChoiceDefault, "", ""},
+    {"Always",
+     "show-avatar-button",
+     "always"},
+    {"Incognito and Guest",
+     "show-avatar-button",
+     "incognito-and-guest"},
+    {"Never",
+     "show-avatar-button",
+     "never"}
+};
+#endif  // CHROME_BROWSER_THORIUM_FLAG_CHOICES_H_
