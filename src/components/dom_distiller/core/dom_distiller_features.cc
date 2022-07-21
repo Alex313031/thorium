365cf24c0f5f2b1a5adecf822ef62db6a863bfc0
Alexander David Frick <Alex313031@gmail.com>
2022-07-21 03:57:32 -0700

    Add files via upload

---
+++ src/components/dom_distiller/core/dom_distiller_features.cc
@@ -0,0 +1,48 @@
+// Copyright 2022 The Chromium Authors and Alex313031. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/dom_distiller/core/dom_distiller_features.h"
+
+#include <string>
+
+#include "base/command_line.h"
+#include "base/metrics/field_trial_params.h"
+#include "components/dom_distiller/core/dom_distiller_switches.h"
+#include "components/dom_distiller/core/pref_names.h"
+#include "components/pref_registry/pref_registry_syncable.h"
+#include "components/prefs/pref_service.h"
+
+namespace dom_distiller {
+
+const base::Feature kReaderMode{"ReaderMode",
+                                base::FEATURE_ENABLED_BY_DEFAULT};
+
+void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
+  // Whether the reader mode option should be shown on distillable pages.
+  registry->RegisterBooleanPref(
+      dom_distiller::prefs::kOfferReaderMode, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+}
+
+bool IsDomDistillerEnabled() {
+  return true;
+}
+
+bool OfferReaderModeInSettings() {
+    return true;
+}
+
+bool ShowReaderModeOption(PrefService* pref_service) {
+  if (OfferReaderModeInSettings())
+    return pref_service->GetBoolean(prefs::kOfferReaderMode);
+  return IsDomDistillerEnabled();
+}
+
+bool ShouldStartDistillabilityService() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kEnableDistillabilityService) ||
+         base::FeatureList::IsEnabled(kReaderMode);
+}
+
+}  // namespace dom_distiller
