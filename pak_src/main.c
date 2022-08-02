429a939b4a3e37a0d726f082e955416c5d38eb67
Alexander David Frick <Alex313031@gmail.com>
2022-08-02 17:27:42 -0500

    Update main.c

--- pak_src/main.c
+++ pak_src/main.c
@@ -7,16 +7,78 @@
 #include "pak_get_file_type.h"
 #include "pak_header.h"
 #include "pak_pack.h"
+#include <stdio.h>
 
-#define HELP_TEXT                                                              \
-    "Pack/Unpack chrome pak file.\n\n"                                         \
-    "%s -u pak_file destination_path\n"                                        \
-    "Unpack chrome pak file at pak_file to destination_path.\n\n"              \
-    "%s -p pak_index_file destination_pak_file\n"                              \
-    "Pack chrome pak file using pak_index_file to destination_pak_file.\n"     \
-    "pak_index_file would be found in unpacked path.\n"                        \
-    "\nNote: existing destination files would be overwritten\n"
+void red () {
+  printf("\033[1;31m");
+}
 
+void yellow () {
+  printf("\033[1;33m");
+}
+
+void cyan () {
+  printf("\033[1;96m");
+}
+
+void green () {
+  printf("\033[1;32m");
+}
+
+void bold () {
+  printf("\033[1m");
+}
+
+void underline () {
+  printf("\033[4m");
+}
+
+void reset () {
+  printf("\033[0m");
+}
+
+
+#define PAK_VERSION_STRING  \
+	"\n"  \
+	"Pak v2.0.1\n\n"  \
+	
+#define CHROMIUM_ASCII  \
+	"\n"  \
+"\033[0m\033[1;34m\033[1m                .,:loool:,.              \n"  \
+"\033[0m\033[1;34m\033[1m            .,coooooooooooooc,.          \n"  \
+"\033[0m\033[1;34m\033[1m         .,lllllllllllllllllllll,.       \n"  \
+"\033[0m\033[1;34m\033[1m        ;ccccccccccccccccccccccccc;      \n"  \
+"\033[0m\033[36m\033[1m      ,\033[0m\033[1;34m\033[1mccccccccccccccccccccccccccccc.    \n"  \
+"\033[0m\033[36m\033[1m     ,oo\033[0m\033[1;34m\033[1mc::::::::ok\033[0m\033[1;37m00000\033[0m\033[37mOOkkkkkkkkkkk:   \n"  \
+"\033[0m\033[36m\033[1m    .ooool\033[0m\033[1;34m\033[1m;;;;:x\033[0m\033[1;37mK0\033[0m\033[1;34mkxxxxxk\033[0m\033[1;37m0X\033[0m\033[37mK0000000000.  \n"  \
+"\033[0m\033[36m\033[1m    :oooool\033[0m\033[1;34m\033[1m;,;O\033[0m\033[1;37mK\033[0m\033[1;34mddddddddddd\033[0m\033[1;37mKX\033[0m\033[37m000000000d  \n"  \
+"\033[0m\033[36m\033[1m    lllllool\033[0m\033[1;34m\033[1m;l\033[0m\033[1;37mN\033[0m\033[1;34mdllllllllllld\033[0m\033[1;37mN\033[0m\033[37mK000000000  \n"  \
+"\033[0m\033[36m\033[1m    lllllllll\033[0m\033[1;34m\033[1mo\033[0m\033[1;37mM\033[0m\033[1;34mdccccccccccco\033[0m\033[1;37mW\033[0m\033[37mK000000000  \n"  \
+"\033[0m\033[36m\033[1m    ;cllllllllX\033[0m\033[1;37mX\033[0m\033[1;34mc:::::::::c\033[0m\033[1;37m0X\033[0m\033[37m000000000d  \n"  \
+"\033[0m\033[36m\033[1m    .ccccllllllO\033[0m\033[1;37mNk\033[0m\033[1;34mc;,,,;cx\033[0m\033[1;37mKK\033[0m\033[37m0000000000.  \n"  \
+"\033[0m\033[36m\033[1m     .cccccclllllxO\033[0m\033[1;37mOOOO0\033[0m\033[36m\033[1mkx\033[0m\033[37mO0000000000;   \n"  \
+"\033[0m\033[36m\033[1m      .:ccccccccllllllllo\033[0m\033[37mO0000000OOO,    \n"  \
+"\033[0m\033[36m\033[1m        ,:ccccccccclllcd\033[0m\033[37m0000OOOOOOl.     \n"  \
+"\033[0m\033[36m\033[1m          .::ccccccccc\033[0m\033[37mdOOOOOOOkx:.       \n"  \
+"\033[0m\033[36m\033[1m            ..,::cccc\033[0m\033[37mxOOOkkko;.          \n"  \
+"\033[0m\033[36m\033[1m               ..::\033[0m\033[37mdOkkxl:.              \n"  \
+"\n"  \
+"\033[1;32m            Long Live Chromium\041\n\033[0m\n"  \
+
+#define HELP_TEXT  \
+	"\n"  \
+    "\033[1;32m\033[4mPak Version 2.0.1\033[0m\033[1;32m - Pack/Unpack Chromium .pak files.\033[0m\n\n"  \
+    "\033[0mUsage:\033[1;96m\n\n"  \
+    "-u \033[1;33m*.pak destination_path\033[0m\n"  \
+    "   Unpack Chromium pak file from '*.pak' file to 'destination_path'.\033[1;96m\n\n"  \
+    "-p \033[1;33mpak_index_file destination_pak_file\033[0m\n"  \
+    "   Re-Pack Chromium pak file using 'pak_index_file' to 'destination_pak_file'.\n"  \
+    "   'pak_index_file' would be found in the path of a previously unpacked file.\033[1;96m\n\n"  \
+    "-v \033[0mPrint version info.\033[1;96m\n\n"  \
+    "-h \033[0mPrint this help info.\033[1;96m\n\n"  \
+    "-c \033[0mPrint some fun Chromium ASCII Art!.\n\n"  \
+	"\033[1;31mNote: Existing destination files will be overwritten!\033[1;33m\n\n"  \
+	
 void printHelp() {
     // get self path
     char selfName[PATH_MAX];
@@ -36,8 +98,56 @@ void printHelp() {
 
     if (ptr != NULL)
         strcpy(selfName, ptr + 1);
-
+	
     printf(HELP_TEXT, selfName, selfName);
+}
+
+
+
+void printVersion() {
+    // get self path
+    char selfName[PATH_MAX];
+#ifdef _WIN32
+    GetModuleFileName(NULL, selfName, PATH_MAX);
+    // get file name from path
+    const char *ptr = strrchr(selfName, '\\');
+#else
+    int ret = readlink("/proc/self/exe", selfName, sizeof(selfName) - 1);
+    if (ret == -1)
+        strcpy(selfName, "pak");
+    else
+        selfName[ret] = 0;
+    // get file name from path
+    const char *ptr = strrchr(selfName, '/');
+#endif
+
+    if (ptr != NULL)
+        strcpy(selfName, ptr + 1);
+	
+    printf(PAK_VERSION_STRING);
+}
+
+void printChromium() {
+    // get self path
+    char selfName[PATH_MAX];
+#ifdef _WIN32
+    GetModuleFileName(NULL, selfName, PATH_MAX);
+    // get file name from path
+    const char *ptr = strrchr(selfName, '\\');
+#else
+    int ret = readlink("/proc/self/exe", selfName, sizeof(selfName) - 1);
+    if (ret == -1)
+        strcpy(selfName, "pak");
+    else
+        selfName[ret] = 0;
+    // get file name from path
+    const char *ptr = strrchr(selfName, '/');
+#endif
+
+    if (ptr != NULL)
+        strcpy(selfName, ptr + 1);
+	
+    printf(CHROMIUM_ASCII);
 }
 
 int pakUnpackPath(char *pakFilePath, char *outputPath) {
@@ -127,6 +237,8 @@ PAK_PACK_INDEX_END:
 #define PAK_FLAGS_HELP 0
 #define PAK_FLAGS_UNPACK 1
 #define PAK_FLAGS_PACK 2
+#define PAK_FLAGS_VERSION 3
+#define PAK_FLAGS_CHROMIUM_ASCII 4
 int main(int argc, char *argv[]) {
     uint32_t flags = 0;
     bool process = false;
@@ -155,6 +267,12 @@ int main(int argc, char *argv[]) {
             case 'x':
                 flags = PAK_FLAGS_UNPACK;
                 break;
+            case 'v':
+                flags = PAK_FLAGS_VERSION;
+                break;
+            case 'c':
+                flags = PAK_FLAGS_CHROMIUM_ASCII;
+                break;
             }
         }
         if ((flags == PAK_FLAGS_UNPACK || flags == PAK_FLAGS_PACK) &&
@@ -165,8 +283,16 @@ int main(int argc, char *argv[]) {
             break;
         }
     }
-    if (flags == PAK_FLAGS_HELP || !process) {
+    if (flags == PAK_FLAGS_VERSION && !process) {
+        printVersion();
+        return 3;
+    }
+    if (flags == PAK_FLAGS_HELP && !process) {
         printHelp();
+        return 0;
+    }
+    if (flags == PAK_FLAGS_CHROMIUM_ASCII && !process) {
+        printChromium();
         return 0;
     }
     if (flags == PAK_FLAGS_UNPACK) {
