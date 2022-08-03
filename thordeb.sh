593c0eccda9889274da3071daae3cf21d83f07cc
Alexander David Frick <Alex313031@gmail.com>
2022-08-03 06:15:09 -0500

    Update thordeb.sh

--- thordeb.sh
+++ thordeb.sh
@@ -6,9 +6,9 @@ YEL='\033[1;33m' # Yellow
 CYA='\033[1;96m' # Cyan
 RED='\033[1;31m' # Red
 GRE='\033[1;32m' # Green
-c0=$'\033[0m' # Reset Text
-bold=$'\033[1m' # Bold Text
-underline=$'\033[4m' # Underline Text
+c0='\033[0m' # Reset Text
+bold='\033[1m' # Bold Text
+underline='\033[4m' # Underline Text
 
 # Error handling
 yell() { echo "$0: $*" >&2; }
