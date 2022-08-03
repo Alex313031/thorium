475b53acc099bafcd6507054f2e82eec9988a8a5
Alexander David Frick <Alex313031@gmail.com>
2022-08-03 06:14:11 -0500

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -6,11 +6,12 @@
 # i.e. cd /home/alex/bin/thorium/
 
 YEL='\033[1;33m' # Yellow
+CYA='\033[1;96m' # Cyan
 RED='\033[1;31m' # Red
 GRE='\033[1;32m' # Green
-c0=$'\033[0m' # Reset Text
-bold=$'\033[1m' # Bold Text
-underline=$'\033[4m' # Underline Text
+c0='\033[0m' # Reset Text
+bold='\033[1m' # Bold Text
+underline='\033[4m' # Underline Text
 
 # Error handling
 yell() { echo "$0: $*" >&2; }
@@ -21,22 +22,22 @@ printf "\n" &&
 printf "${YEL}Cleaning up build artifacts...\n" &&
 printf "${RED}\n" &&
 
-cd ~/chromium/src/out/thorium
+cd ~/chromium/src/out/thorium &&
 
-find . -name "*deps*" -delete
-find . -name "*TOC*" -delete
-rm -r pyproto
-rm -r obj
-rm -r newlib_pnacl_nonsfi
-rm -r newlib_pnacl
-rm -r nacl_bootstrap_x64
-rm -r irt_x64
-rm -r glibc_x64
-rm -r gen
-rm -r etc
-rm -r clang_newlib_x64
-rm -r thinlto-cache
-rm -r fontconfig_caches
+find . -name "*deps*" -delete &&
+find . -name "*TOC*" -delete &&
+rm -r -v pyproto &&
+rm -r -v obj &&
+rm -r -v newlib_pnacl_nonsfi &&
+rm -r -v newlib_pnacl &&
+rm -r -v nacl_bootstrap_x64 &&
+rm -r -v irt_x64 &&
+rm -r -v glibc_x64 &&
+rm -r -v gen &&
+rm -r -v etc &&
+rm -r -v clang_newlib_x64 &&
+rm -r -v thinlto-cache &&
+rm -r -v fontconfig_caches &&
 
 printf "${GRE}Done cleaning artifacts.\n" &&
 tput sgr0
