8d5509eb53db70a002e7e7537c111256e9cb7ce5
Alexander David Frick <Alex313031@gmail.com>
2022-08-30 23:45:58 -0500

    Update build_debug_linux.sh

--- infra/DEBUG/build_debug_linux.sh
+++ infra/DEBUG/build_debug_linux.sh
@@ -34,7 +34,7 @@ printf "${CYA}\n" &&
 # Build Thorium and Thorium UI Debug Shell
 export NINJA_SUMMARIZE_BUILD=1 &&
 
-../../depot_tools/autoninja -C ~/chromium/src/out/thorium chrome chrome_sandbox chromedriver thorium_shell thorium_ui_debug_shell -j$@ &&
+../../depot_tools/autoninja -C ~/chromium/src/out/thorium chrome chrome_sandbox chromedriver thorium_shell thorium_ui_debug_shell clear_key_cdm -j$@ &&
 
 mkdir -v -p ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell &&
 mkdir -v -p ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell/lib &&
