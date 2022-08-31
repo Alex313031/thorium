bbe6691df9310901caa7ce1cbbed23274f19c538
Alexander David Frick <Alex313031@gmail.com>
2022-08-30 23:46:31 -0500

    Update build_debug_shell_win.sh

--- infra/DEBUG/build_debug_shell_win.sh
+++ infra/DEBUG/build_debug_shell_win.sh
@@ -36,8 +36,8 @@ export NINJA_SUMMARIZE_BUILD=1 &&
 
 ../../depot_tools/autoninja -C ~/chromium/src/out/thorium thorium_ui_debug_shell -j$@ &&
 
-mkdir -v ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell &&
-mkdir -v ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell/icons &&
+mkdir -v -p ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell &&
+mkdir -v -p ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell/icons &&
 
 cp -r -f -v ./icons/icon_16.png ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell/icons &&
 cp -r -f -v ./icons/icon_24.png ~/chromium/src/out/thorium/Thorium_UI_Debug_Shell/icons &&
