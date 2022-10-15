7eaea2961f04ee1b6fd9d72dc894d039e6d34354
Alexander David Frick <Alex313031@gmail.com>
2022-10-14 21:12:35 -0500

    Update browser_commands.cc

--- src/chrome/browser/ui/browser_commands.cc
+++ src/chrome/browser/ui/browser_commands.cc
@@ -1,4 +1,4 @@
-// Copyright 2022 The Chromium Authors and Alex313031. All rights reserved.
+// Copyright 2022 The Chromium Authors and Alex313031
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -93,7 +93,6 @@
 #include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
-#include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/content_restriction.h"
 #include "chrome/common/pref_names.h"
