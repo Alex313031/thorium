46893f326c42b08a56769f3de6743aec049b2091
midzer <midzer@gmail.com>
2022-10-22 12:20:39 +0200

    support AC3 and E-AC3

--- src/third_party/ffmpeg/chromium/scripts/build_ffmpeg.py
+++ src/third_party/ffmpeg/chromium/scripts/build_ffmpeg.py
@@ -1001,9 +1001,9 @@ def ConfigureAndBuild(target_arch, target_os, host_os, host_arch, parallel_jobs,
 
   # Google Chrome & ChromeOS specific configuration.
   configure_flags['Chrome'].extend([
-      '--enable-decoder=aac,h264,hevc',
-      '--enable-demuxer=aac',
-      '--enable-parser=aac,h264,hevc',
+      '--enable-decoder=aac,h264,mp3,eac3,ac3,hevc,mpeg4,mpegvideo,mp2,mp1,flac',
+      '--enable-demuxer=aac,mp3,mov,dtshd,dts,avi,mpegvideo,m4v,h264,vc1,flac',
+      '--enable-parser=aac,h264,mpegaudio,mpeg4video,mpegvideo,ac3,h261,vc1,h263,flac',
   ])
 
   # Google ChromeOS specific configuration.
