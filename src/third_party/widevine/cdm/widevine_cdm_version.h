35044fc7df341d62872647f62340347b456394a7
Alexander David Frick <Alex313031@gmail.com>
2023-01-25 05:03:23 -0600

    Update widevine_cdm_version.h

--- src/third_party/widevine/cdm/widevine_cdm_version.h
+++ src/third_party/widevine/cdm/widevine_cdm_version.h
@@ -8,10 +8,13 @@
 #ifndef WIDEVINE_CDM_VERSION_H_
 #define WIDEVINE_CDM_VERSION_H_
 
+#include "third_party/widevine/cdm/widevine_cdm_common.h"
+
 // If the Widevine CDM is available define the following:
 //  - WIDEVINE_CDM_VERSION_STRING (with the version of the CDM that's available
 //    as a string, e.g., "1.0.123.456").
 
+#define WIDEVINE_CDM_AVAILABLE
 #define WIDEVINE_CDM_VERSION_STRING "undefined"
 
 #endif  // WIDEVINE_CDM_VERSION_H_
