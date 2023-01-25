1057fb70f43780ba0cd052407074a6b9330ea1d7
Alexander David Frick <Alex313031@gmail.com>
2023-01-25 00:47:34 -0600

    Add files via upload

---
+++ logos/NEW/mac/gen/README.md
@@ -0,0 +1,10 @@
+## ICNS generation directory. <img src="https://github.com/Alex313031/Thorium/blob/main/logos/NEW/mac/icon_2048px.png" width="48">
+
+This directory contains files/scripts for generating an .icns (IconSet) file for MacOS from predefined icon .PNGs from 16px to 1024px.
+
+ - In the app and document subdirs, there are the icon files and two scripts, both named build_icns.sh, which must each be seperately run in their respective folders.
+They will generate `Thorium.icns` and `ThoriumDocument.icns`, which should both be placed in the parent of this directory, I.E. //thorium/logos/NEW/mac/
+
+They will then be renamed to app.icns and document.icns and copied to //thorium/chrome/app/theme/chromium/mac/ for proper Thorium branding on MacOS.
+
+<img src="https://github.com/Alex313031/Thorium/blob/main/logos/NEW/mac/apple.png" width="200">
---
+++ logos/NEW/mac/gen/app/build_icns.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Copyright (c) 2022 Alex313031.
+
+YEL='\033[1;33m' # Yellow
+RED='\033[1;31m' # Red
+GRE='\033[1;32m' # Green
+c0=$'\033[0m' # Reset Text
+bold=$'\033[1m' # Bold Text
+underline=$'\033[4m' # Underline Text
+
+# Error handling
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "${RED}Failed $*"; }
+
+printf "\n" &&
+printf "${YEL}Generating Thorium App .icns file for MacOS...\n" &&
+printf "${GRE}\n" &&
+
+# Copy .pngs
+export output_iconset_name="Thorium.iconset" &&
+
+mkdir $output_iconset_name &&
+
+cp -v ./icon_16x16.png ./Thorium.iconset/ &&
+cp -v ./icon_16x16@2x.png ./Thorium.iconset/ &&
+cp -v ./icon_32x32.png ./Thorium.iconset/ &&
+cp -v ./icon_32x32@2x.png ./Thorium.iconset/ &&
+cp -v ./icon_64x64.png ./Thorium.iconset/ &&
+cp -v ./icon_64x64@2x.png ./Thorium.iconset/ &&
+cp -v ./icon_128x128.png ./Thorium.iconset/ &&
+cp -v ./icon_128x128@2x.png ./Thorium.iconset/ &&
+cp -v ./icon_256x256.png ./Thorium.iconset/ &&
+cp -v ./icon_256x256@2x.png ./Thorium.iconset/ &&
+cp -v ./icon_512x512.png ./Thorium.iconset/ &&
+cp -v ./icon_512x512@2x.png ./Thorium.iconset/ &&
+
+# Generate .icns file
+iconutil -c icns $output_iconset_name &&
+
+# Remove temp dir
+rm -R -v $output_iconset_name
+
+printf "\n" &&
+printf "${GRE}Done!\n" &&
+tput sgr0
