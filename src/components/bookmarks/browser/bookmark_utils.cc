9814b73104c826658433fe5c856c8f6e6e2b5cb6
Alexander David Frick <Alex313031@gmail.com>
2023-01-29 01:31:54 -0600

    Update bookmark_utils.cc

--- src/components/bookmarks/browser/bookmark_utils.cc
+++ src/components/bookmarks/browser/bookmark_utils.cc
@@ -443,7 +443,7 @@ bool DoesBookmarkContainWords(const std::u16string& title,
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(
-      prefs::kShowBookmarkBar, false,
+      prefs::kShowBookmarkBar, true,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(prefs::kEditBookmarksEnabled, true);
   registry->RegisterBooleanPref(
