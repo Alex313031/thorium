5f1928a9410aad6b020b3545fc5ac8d307944137
Alexander David Frick <Alex313031@gmail.com>
2023-02-26 21:15:11 -0600

    restore libjxl

--- src/third_party/blink/common/features.cc
+++ src/third_party/blink/common/features.cc
@@ -711,6 +711,9 @@ BASE_FEATURE(kCacheCodeOnIdle,
 const base::FeatureParam<int> kCacheCodeOnIdleDelayParam{&kCacheCodeOnIdle,
                                                          "delay-in-ms", 0};
 
+// Enables the JPEG XL Image File Format (JXL).
+BASE_FEATURE(kJXL, "JXL", base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Make all pending 'display: auto' web fonts enter the swap or failure period
 // immediately before reaching the LCP time limit (~2500ms), so that web fonts
 // do not become a source of bad LCP.
