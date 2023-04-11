1b04d2df3bd27e1bd78cd0a9d624d2ac8486b05c
Alexander Frick <Alex313031@gmail.com>
2023-04-11 14:25:02 -0500

    flag and v8 opts

--- src/components/flags_ui/flags_state.cc
+++ src/components/flags_ui/flags_state.cc
@@ -596,9 +596,6 @@ void FlagsState::GetFlagFeatureEntries(
   for (const FeatureEntry& entry : feature_entries_) {
     std::string desc = entry.visible_description;
     if (skip_feature_entry.Run(entry))
-      if (flags::IsFlagExpired(flags_storage, entry.internal_name))
-        desc.insert(0, "!¡ NOTE: THIS FLAG IS EXPIRED AND MAY STOP FUNCTIONING OR BE REMOVED SOON ¡! ");
-      else
       continue;
 
     base::Value::Dict data;
