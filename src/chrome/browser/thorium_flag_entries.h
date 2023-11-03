3759529384a18ae7c1d59155e1fef48573e5834c
Alexander Frick <Alex313031@gmail.com>
2023-11-03 10:05:53 -0500

    fix args

--- src/chrome/browser/thorium_flag_entries.h
+++ src/chrome/browser/thorium_flag_entries.h
@@ -17,6 +17,14 @@
      kOsLinux, SINGLE_VALUE_TYPE("auto-dark-mode")},
 #endif // BUILDFLAG(IS_LINUX)
 
+#if BUILDFLAG(IS_WIN)
+    {"disable-aero",
+     "Disable Aero Window Frame Compositing",
+     "Use the classic Chrome theme designed to mimick \"Aero\" window controls. "
+     "Typically used when desktop composition is disabled or unavailable.";
+     kOsWin, SINGLE_VALUE_TYPE("disable-aero")},
+#endif // BUILDFLAG(IS_WIN)
+
     {"force-high-contrast",
      "Enable High Contrast Mode",
      "Enables high contrast mode for all Thorium instances.",
