e061852f468775704ceefe749e596c68523f4c0a
Alexander Frick <Alex313031@gmail.com>
2024-11-10 02:24:28 -0600

    M130 pre-stage

--- src/chrome/installer/launcher_support/chrome_launcher_support.cc
+++ src/chrome/installer/launcher_support/chrome_launcher_support.cc
@@ -28,7 +28,7 @@ const wchar_t kUpdateClientsRegKey[] = L"Software\\Google\\Update\\Clients";
 const wchar_t kBrowserAppGuid[] = L"{8A69D345-D564-463c-AFF1-A69D9E530F96}";
 const wchar_t kSxSBrowserAppGuid[] = L"{4ea16ac7-fd5a-47c3-875b-dbf4a2008c20}";
 #else
-const wchar_t kInstallationRegKey[] = L"Software\\Chromium";
+const wchar_t kInstallationRegKey[] = L"Software\\Thorium";
 #endif
 
 // Copied from util_constants.cc.
--- upstream_version.sh
+++ upstream_version.sh
@@ -36,7 +36,7 @@ else
     export CR_SRC_DIR
 fi
 
-CR_VER="128.0.6613.194"
+CR_VER="130.0.6723.125"
 
 export CR_VER &&
 
