80a9ababc5a3ce782c6386493bd940ca1862207c
Alexander Frick <Alex313031@gmail.com>
2024-11-24 15:08:59 -0600

    .142 minor uprev

--- upstream_version.sh
+++ upstream_version.sh
@@ -36,7 +36,7 @@ else
     export CR_SRC_DIR
 fi
 
-CR_VER="130.0.6723.141"
+CR_VER="130.0.6723.142"
 
 export CR_VER &&
 
--- version.sh
+++ version.sh
@@ -36,7 +36,7 @@ else
     export CR_SRC_DIR
 fi
 
-THOR_VER="130.0.6723.141"
+THOR_VER="130.0.6723.142"
 
 export THOR_VER &&
 
