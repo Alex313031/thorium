9435bd12097602f6617dbf24ed26776e45fff286
Alexander Frick <Alex313031@gmail.com>
2024-12-26 16:07:31 -0600

    fix renaming of mini_installer

--- build_win.sh
+++ build_win.sh
@@ -49,7 +49,7 @@ cd ${CR_SRC_DIR} &&
 autoninja -C out/thorium thorium_all -j$@ &&
 autoninja -C out/thorium setup mini_installer -j$@ &&
 
-mv -v -f ${CR_SRC_DIR}/out/thorium/mini_installer.exe ${CR_SRC_DIR}/out/thorium/thorium_mini_installer.exe &&
+#mv -v -f ${CR_SRC_DIR}/out/thorium/mini_installer.exe ${CR_SRC_DIR}/out/thorium/thorium_mini_installer.exe &&
 
 printf "\n" &&
 cat ~/thorium/logos/thorium_logo_ascii_art.txt &&
