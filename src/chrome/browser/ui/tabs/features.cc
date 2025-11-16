6314666b3ccf3aa960bc794c34b64d7abf760459
Alexander Frick <Alex313031@gmail.com>
2025-11-16 14:35:33 -0600

    fix files I didn't rebase properly, add missing file to thorium dev bookmarks

--- src/chrome/browser/ui/tabs/features.cc
+++ src/chrome/browser/ui/tabs/features.cc
@@ -6,6 +6,7 @@
 
 #include "base/feature_list.h"
 #include "chrome/browser/buildflags.h"
+#include "chrome/browser/ui/ui_features.h"
 
 namespace tabs {
 
@@ -16,6 +17,12 @@ BASE_FEATURE(kSplitTabStrip,
              "SplitTabStrip",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables tabs to scroll in the tabstrip. https://crbug.com/951078
+BASE_FEATURE(kScrollableTabStrip,
+             "ScrollableTabStrip",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+const char kMinimumTabWidthFeatureParameterName[] = "minTabWidth";
+
 // Enables tab scrolling while dragging tabs in tabstrip
 // https://crbug.com/1145747
 BASE_FEATURE(kScrollableTabStripWithDragging,
@@ -23,11 +30,29 @@ BASE_FEATURE(kScrollableTabStripWithDragging,
              base::FEATURE_ENABLED_BY_DEFAULT);
 const char kTabScrollingWithDraggingModeName[] = "tabScrollWithDragMode";
 
+// Enables different methods of overflow when scrolling tabs in tabstrip
+// https://crbug.com/951078
+BASE_FEATURE(kScrollableTabStripOverflow,
+             "kScrollableTabStripOverflow",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+const char kScrollableTabStripOverflowModeName[] = "tabScrollOverflow";
+
+BASE_FEATURE(kTabGroupHome, "TabGroupHome", base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kTabSearchPositionSetting,
              "TabSearchPositionSetting",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// TODO: Alex313031 Maybe disable?
+BASE_FEATURE(kTabGroupShortcuts,
+             "TabGroupShortcuts",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 bool CanShowTabSearchPositionSetting() {
+  // Alternate tab search locations cannot be repositioned.
+  if (features::IsTabSearchMoving()) {
+    return false;
+  }
 // Mac and other platforms will always have the tab search position in the
 // correct location, cros/linux/win git the user the option to change.
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
@@ -35,6 +60,10 @@ bool CanShowTabSearchPositionSetting() {
 #else
   return false;
 #endif
+}
+
+bool AreTabGroupShortcutsEnabled() {
+  return base::FeatureList::IsEnabled(kTabGroupShortcuts);
 }
 
 }  // namespace tabs
