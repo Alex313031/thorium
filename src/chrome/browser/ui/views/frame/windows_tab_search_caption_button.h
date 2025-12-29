f488c0835dde1400edf153c1e8317d9362019f8a
Alexander Frick <Alex313031@gmail.com>
2025-12-29 13:05:50 -0600

    remove windows caption button tab search for now, fix kDefaultMenuShowDelay issue

---
+++ src/chrome/browser/ui/views/frame/windows_tab_search_caption_button.h
@@ -0,0 +1,37 @@
+// Copyright 2025 The Chromium Authors and Alex313031
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_FRAME_WINDOWS_TAB_SEARCH_CAPTION_BUTTON_H_
+#define CHROME_BROWSER_UI_VIEWS_FRAME_WINDOWS_TAB_SEARCH_CAPTION_BUTTON_H_
+
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/windows_caption_button.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+
+class BrowserFrameViewWin;
+class TabSearchBubbleHost;
+
+class WindowsTabSearchCaptionButton : public WindowsCaptionButton {
+  METADATA_HEADER(WindowsTabSearchCaptionButton, WindowsCaptionButton)
+
+ public:
+  WindowsTabSearchCaptionButton(BrowserFrameViewWin* frame_view,
+                                BrowserView* browser_view,
+                                base::WeakPtr<TabStrip> tab_strip,
+                                ViewID button_type,
+                                const std::u16string& accessible_name);
+  WindowsTabSearchCaptionButton(const WindowsTabSearchCaptionButton&) = delete;
+  WindowsTabSearchCaptionButton& operator=(
+      const WindowsTabSearchCaptionButton&) = delete;
+  ~WindowsTabSearchCaptionButton() override;
+
+  TabSearchBubbleHost* tab_search_bubble_host() {
+    return tab_search_bubble_host_.get();
+  }
+
+ private:
+  std::unique_ptr<TabSearchBubbleHost> tab_search_bubble_host_;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_FRAME_WINDOWS_TAB_SEARCH_CAPTION_BUTTON_H_
