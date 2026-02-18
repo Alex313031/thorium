fc6ba419ba9b1e40a25480041f059082f33c95f3
Alexander Frick <Alex313031@gmail.com>
2026-02-17 22:44:24 -0600

    make make_portable_win.sh pick any matching thorium_*_installer.exe name

--- infra/portable/make_portable_win.sh
+++ infra/portable/make_portable_win.sh
@@ -44,7 +44,7 @@ sleep 2 &&
 # Extract data.tar.xz
 mkdir -v -p ./temp &&
 mkdir -v -p ./temp/USER_DATA &&
-7z x thorium_mini_installer.exe &&
+7z x thorium_*_installer.exe &&
 7z x chrome.7z &&
 mv -v Chrome-bin ./temp/BIN &&
 cp -r -v ./README.win temp/README.txt &&
