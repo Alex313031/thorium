1e551341e4e5ab319541abe111ab486692574c30
Alexander Frick <Alex313031@gmail.com>
2026-02-23 21:58:09 -0600

    fix incorrectly rebased dom_distiller_features.cc for Android compilation

--- src/components/dom_distiller/core/dom_distiller_features.cc
+++ src/components/dom_distiller/core/dom_distiller_features.cc
@@ -11,6 +11,12 @@
 #include "components/dom_distiller/core/dom_distiller_switches.h"
 #include "components/dom_distiller/core/pref_names.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/feature_map.h"
+#include "base/no_destructor.h"
+#include "components/dom_distiller/core/android/jni_headers/DomDistillerFeatureMap_jni.h"
+#endif
+
 namespace dom_distiller {
 
 bool IsDomDistillerEnabled() {
@@ -26,5 +32,26 @@ bool ShouldStartDistillabilityService() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kEnableDistillabilityService) || kReaderModeDesktop;
 }
+
+#if BUILDFLAG(IS_ANDROID)
+// Feature declarations below -- alphabetical order.
+BASE_FEATURE(kReaderModeAutoDistill,
+             "ReaderModeAutoDistill",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kReaderModeImprovements,
+             "ReaderModeImprovements",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+namespace android {
+static jlong JNI_DomDistillerFeatureMap_GetNativeMap(JNIEnv* env) {
+  static const base::Feature* const kFeaturesExposedToJava[] = {
+      &kReaderModeAutoDistill, &kReaderModeImprovements};
+  static base::NoDestructor<base::android::FeatureMap> kFeatureMap(
+      kFeaturesExposedToJava);
+  return reinterpret_cast<jlong>(kFeatureMap.get());
+}
+}  // namespace android
+#endif  // BUILDFLAG(IS_ANDROID)
 
 }  // namespace dom_distiller
